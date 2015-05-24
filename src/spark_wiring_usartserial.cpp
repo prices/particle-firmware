c1f41b628fc55abc38b6e60122ee0c46d37420fb
Scott Price <prices@hugllc.com>
2015-05-24 16:12:00 -0500

    Some changes for our code.

--- src/spark_wiring_usartserial.cpp
+++ src/spark_wiring_usartserial.cpp
@@ -134,6 +134,11 @@ void USARTSerial::begin(unsigned long baud)
 	USARTSerial_Enabled = true;
 	transmitting = false;
 }
+void USARTSerial::beginonewire(unsigned long baud)
+{
+    USARTSerial::begin(baud);
+    pinMode(usartMap->usart_tx_pin, AF_OUTPUT_DRAIN);
+}
 
 // TODO
 void USARTSerial::begin(unsigned long baud, byte config)
